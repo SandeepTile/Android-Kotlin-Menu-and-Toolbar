0632fe376724be59f167812542b2504654687983
sandeeptile <sandeeptile@gmail.com>
2018-08-23 22:11:46 +0530

    customize toolbar added

--- app/src/main/java/com/example/sandy/kotlin_menu_and_toolbar/ToolbarActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_menu_and_toolbar/ToolbarActivity.kt
@@ -2,6 +2,7 @@ package com.example.sandy.kotlin_menu_and_toolbar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_toolbar.*
 
 class ToolbarActivity : AppCompatActivity() {
 
@@ -9,7 +10,10 @@ class ToolbarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toolbar)
 
-        supportActionBar!!.setTitle("Add post here")
+        /*for hide default toolbar customize style.xml*/
+        setSupportActionBar(toolBar)
+       // supportActionBar!!.setTitle("Add post here")
         supportActionBar!!.setDisplayHomeAsUpEnabled(true) //for this method config AndroidManifest.xml
     }
 }
+
