eab253e57c7cbb6518b1710ddbad5384aa08f092
sandeeptile <sandeeptile@gmail.com>
2018-08-23 11:43:31 +0530

    menu created

--- app/src/main/java/com/example/sandy/kotlin_menu_and_toolbar/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_menu_and_toolbar/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.sandy.kotlin_menu_and_toolbar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        Toast.makeText(this@MainActivity,item!!.title.toString(),Toast.LENGTH_LONG).show()
+
+        return super.onOptionsItemSelected(item)
     }
 }
