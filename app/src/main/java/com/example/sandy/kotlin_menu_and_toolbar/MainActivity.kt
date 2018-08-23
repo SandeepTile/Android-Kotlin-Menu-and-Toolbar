ea94b9df92574c39c5fca125a029f040a4d4fb54
sandeeptile <sandeeptile@gmail.com>
2018-08-23 11:19:10 +0530

    first commit

---
+++ app/src/main/java/com/example/sandy/kotlin_menu_and_toolbar/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sandy.kotlin_menu_and_toolbar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
